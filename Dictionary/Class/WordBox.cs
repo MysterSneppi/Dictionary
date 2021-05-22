24763a5a41caa3f3ae4741b47fd824091e6e2981
Alex-ViGiL <60942937+Alex-ViGiL@users.noreply.github.com>
2021-05-22 20:43:51 +0300

    f

---
+++ Dictionary/Class/WordBox.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Dictionary.Class
+{
+    class WordBox
+    {
+        public Border createText(string firstWord,string secondWord) 
+        {
+            Border border = new Border
+            {
+                Height = 80,
+                Background = new SolidColorBrush(Colors.Aqua),
+                Margin = new Thickness(0,5,0,0),
+                BorderBrush = new SolidColorBrush(Colors.Black),
+                BorderThickness = new Thickness(5)
+
+            };
+
+            StackPanel stackPanel = new StackPanel { Orientation = Orientation.Horizontal};
+
+            TextBox textBlock = new TextBox {Text = firstWord , FontSize = 20 , MaxLength = 40 , Height = 40, HorizontalAlignment = HorizontalAlignment.Center, Margin = new Thickness(10, 0, 6, 0) };
+            TextBlock textBlock1 = new TextBlock { Text = "-----------", FontSize = 20 , HorizontalAlignment = HorizontalAlignment.Center };
+            TextBox textBlock2 = new TextBox { Text = secondWord, FontSize = 20, MaxLength = 40 , Height = 40, HorizontalAlignment = HorizontalAlignment.Center , Margin = new Thickness(6, 0, 0, 10) };
+
+            stackPanel.Children.Add(textBlock);
+            stackPanel.Children.Add(textBlock1);
+            stackPanel.Children.Add(textBlock2);
+
+            border.Child = stackPanel;
+
+            return border;
+
+        }
+
+    }
+}
---
+++ Dictionary/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using Dictionary.Class;
+using System.Windows;
+
+namespace Dictionary
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WordBox wordBox = new WordBox();
+
+        int num = 0;//Текущее количество слов
+        int max = 50;//Максимальное количество слов
+        int money = 100;//Количество денег
+        int priceMax = 5;//Цена добавления 
+
+        //Метод для обновления информации на екране
+        public void menu()
+        {
+            NUM.Text = $"{num}";//Присваиваем текст из  Num
+            LVL.Text = $"{num / 10}"; //Определяем уровень и выводим на экран
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            MAX.Text = $"{max}";
+            Money.Text = $"{money}$";
+        }
+
+        //Кнопка сохранения
+        private void Save(object sender, RoutedEventArgs e)
+        {
+
+            if (FirstWord.Text == "" || SecondWord.Text == "")
+            {
+                MessageBox.Show("Поля пустые");
+                return;
+            }
+            else
+            {
+                if (num < max)
+                {
+                    //Добавляем дочерний елемент для списка слов
+                    List.Children.Add(wordBox.createText(FirstWord.Text, SecondWord.Text));
+
+                    //Делаем поле пусым
+                    FirstWord.Text = "";
+                    SecondWord.Text = "";
+                    num++;
+                    menu();
+                }
+                else { MessageBox.Show("Недостаточно места"); }
+
+            }
+
+            
+        }
+
+        //Добавление +10 к максимальному количеству слов
+        private void add10(object sender, RoutedEventArgs e)
+        {
+
+            if (money >= priceMax)
+            {
+                max += 10;
+                money -= 5;
+                MAX.Text = $"{max}";
+                Money.Text = $"{money}$";
+            }
+            else 
+            {
+                MessageBox.Show("Недостаточно средств для покупки");
+            }
+            
+        }
+    }
+}
